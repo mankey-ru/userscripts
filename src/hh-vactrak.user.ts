ce9556aa2e207c1b39fd7cc17a670d3aa87c5e7e
mankey-ru <job@mankey.ru>
2026-07-13 20:45:46 +0700

    version bump

--- src/hh-vactrak.user.ts
+++ src/hh-vactrak.user.ts
@@ -3,7 +3,7 @@
 // @description  Reloads the page every N minutes and alerts you if there are new vacancies on the page since the last check. It uses localStorage to remember which vacancies have already been seen.
 // @author       mankey-ru
 // @namespace    mankey-ru/hh-vactrak
-// @version      1.70
+// @version      1.71
 // @match        https://hh.ru/search/vacancy?*
 // @match        https://hh.uz/search/vacancy?*
 // @match        https://rabota.by/search/vacancy?*
